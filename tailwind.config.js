ce7b82a7b094d08b51fba47b7f517a826a249a9e
Pkay376 <puritykidiavai@gmail.com>
2024-01-16 20:13:16 +0300

    Updated

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "370px",
+      // => @media (min-width: 370px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+
+    plugins: [],
+  },
+};
